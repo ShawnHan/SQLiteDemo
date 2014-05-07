a62bee172fff942a72ae0fa99030832f227c4329
Shawn <hanlong522@gmail.com>
2014-05-07 13:48:29 +0800

    Initial Commit

---
+++ FailedBanks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FailedBanks
+//
+//  Created by Shawn on 14-5-7.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FailedBanksTests/FailedBanksTests.m
@@ -0,0 +1,34 @@
+//
+//  FailedBanksTests.m
+//  FailedBanksTests
+//
+//  Created by Shawn on 14-5-7.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FailedBanksTests : XCTestCase
+
+@end
+
+@implementation FailedBanksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
